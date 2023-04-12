8daad5203e4e8b92795a1082e7f2581801f542d4
Enrique Martínez-Meyer <emm@ib.unam.mx>
2023-04-11 22:30:42 -0600

    Update Recorte_CRU.R

--- Recorte_CRU.R
+++ Recorte_CRU.R
@@ -49,7 +49,7 @@ library(terra)
 # Crear el mapa de la CRU a partir del shp
 CRU_LL <- rgdal::readOGR(dsn = "Lim_CRU/", layer = "CRU_LL")
 
-#### Temperatura Media
+#### Temperatura Media-----------------------------------------------------------------------------------------------------
 
 # Llamar a las capas raster de Chelsa por variable y crear una lista: Temperatura Media Anual (TAS)
 path_TAS <- list.files("/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Geodatos/Mundo/Chelsa_v21_month/tas",
@@ -62,13 +62,13 @@ CRU_TAS <- raster::mask(crop(TAS_stk, CRU_LL), CRU_LL)
 plot(CRU_TAS[[1]])
 CRU_TAS_Cels <- round((CRU_TAS/10)-273.15, 2)
 plot(CRU_TAS_Cels[[1]])
-CRU_TAS_stk <- stack(CRU_TAS_Cels)
+CRU_TAS_stk <- stack(CRU_TAS_Cels) # Se tiene que convertir el brick en stack para los siguientes procesos
 
 # Generar el csv del índice de capas del stack
 CRU_capasID <- as_tibble(names(CRU_TAS_stk)) %>%
   mutate(dimindex = row_number()) %>%
   relocate(dimindex, Capa = value)
-write_csv(CRU_capasID, "/Users/enriquemm/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_capasID.csv")
+write_csv(CRU_capasID, "/Users/enriquemm/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_TMd_capasID.csv")
 
 # Crear una tabla separando año y mes del nombre de las capas
 capas_CRU_stk <- as_tibble(names(CRU_TAS_stk)) %>%
@@ -86,16 +86,103 @@ CRU_TMd_Completa <- CRU_TAS_tbbl %>%
 
 # Seleccionar solo los campos de año, mes y temperatura, exportarlo a csv
 CRU_SerT_TMd <- dplyr::select(CRU_TMd_Completa, -CapaID, -Long, -Lat)
-write_csv(CRU_SerT_TMd, "/Users/enriquemm/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_SerT_Tmd.csv")
+write_csv(CRU_SerT_TMd, "/Users/enriquemm/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_SerT_TMd.csv")
 
-# Generar el csv del índice de coordenadas
+
+
+####--------------------------------------------------------------------------------------------------------------------------
+
+# Generar el csv del índice de coordenadas. Este archivo se genera con TMd pero sirve para todas las variables 
 CRU_Coordenadas <- CRU_TMd_Completa %>%
   filter(Anho == 1980, Mes == 1) %>%
   select(CellID, Long, Lat)
 write_csv(CRU_Coordenadas, "/Users/enriquemm/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_Coordenadas.csv")
 
 
-##### Precipitación
+
+#### Temperatura Mínima-----------------------------------------------------------------------------------------------------
+
+# Llamar a las capas raster de Chelsa por variable y crear una lista: Temperatura Mínima (TMn)
+path_TMn <- list.files("/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Geodatos/Mundo/Chelsa_v21_month/tasmin",
+                       pattern = "*.tif",full.names = TRUE)
+
+TMn_stk <- raster::stack(path_TMn) # Crear un stack de las capas
+
+# Recortar el stack de capas TMn a la CRU
+CRU_TMn <- raster::mask(crop(TMn_stk, CRU_LL), CRU_LL)
+plot(CRU_TMn[[1]])
+CRU_TMn_Cels <- round((CRU_TMn/10)-273.15, 2)
+plot(CRU_TMn_Cels[[1]])
+CRU_TMn_stk <- stack(CRU_TMn_Cels) # Se tiene que convertir el brick en stack para los siguientes procesos
+
+# Generar el csv del índice de capas del stack
+CRU_TMn_capasID <- as_tibble(names(CRU_TMn_stk)) %>%
+  mutate(dimindex = row_number()) %>%
+  relocate(dimindex, Capa = value)
+write_csv(CRU_TMn_capasID, "/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_TMn_capasID.csv")
+
+# Crear una tabla separando año y mes del nombre de las capas
+capas_TMn_CRU_stk <- as_tibble(names(CRU_TMn_stk)) %>%
+  mutate(dimindex = row_number()) %>%
+  relocate(dimindex, Capa = value) %>%
+  separate(Capa, c(NA, NA, "Mes", "Anho", NA), sep = "_", convert = T)
+
+# Convertir el stack en un tibble y unir todos los campos
+CRU_TMn_tbbl <- na.omit(tabularaster::as_tibble(CRU_TMn_stk, xy=TRUE, dim=TRUE,
+                                                cell=TRUE, value=TRUE))
+CRU_TMn_Completa <- CRU_TMn_tbbl %>%
+  left_join(capas_TMn_CRU_stk, by = "dimindex") %>%
+  relocate(CellID = cellindex, CapaID = dimindex, Long = x, Lat = y, Anho, Mes, TMn =
+             cellvalue)
+
+# Seleccionar solo los campos de año, mes y temperatura, exportarlo a csv
+CRU_SerT_TMn <- dplyr::select(CRU_TMn_Completa, -CapaID, -Long, -Lat)
+write_csv(CRU_SerT_TMn, "/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_SerT_TMn.csv")
+
+
+
+#### Temperatura Máxima-----------------------------------------------------------------------------------------------------
+
+# Llamar a las capas raster de Chelsa por variable y crear una lista: Temperatura Mínima (TMx)
+path_TMx <- list.files("/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Geodatos/Mundo/Chelsa_v21_month/tasmax",
+                       pattern = "*.tif",full.names = TRUE)
+
+TMx_stk <- raster::stack(path_TMx) # Crear un stack de las capas
+
+# Recortar el stack de capas TMx a la CRU
+CRU_TMx <- raster::mask(crop(TMx_stk, CRU_LL), CRU_LL)
+plot(CRU_TMx[[1]])
+CRU_TMx_Cels <- round((CRU_TMx/10)-273.15, 2)
+plot(CRU_TMx_Cels[[1]])
+CRU_TMx_stk <- stack(CRU_TMx_Cels) # Se tiene que convertir el brick en stack para los siguientes procesos
+
+# Generar el csv del índice de capas del stack
+CRU_TMx_capasID <- as_tibble(names(CRU_TMx_stk)) %>%
+  mutate(dimindex = row_number()) %>%
+  relocate(dimindex, Capa = value)
+write_csv(CRU_TMx_capasID, "/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_TMx_capasID.csv")
+
+# Crear una tabla separando año y mes del nombre de las capas
+capas_TMx_CRU_stk <- as_tibble(names(CRU_TMx_stk)) %>%
+  mutate(dimindex = row_number()) %>%
+  relocate(dimindex, Capa = value) %>%
+  separate(Capa, c(NA, NA, "Mes", "Anho", NA), sep = "_", convert = T)
+
+# Convertir el stack en un tibble y unir todos los campos
+CRU_TMx_tbbl <- na.omit(tabularaster::as_tibble(CRU_TMx_stk, xy=TRUE, dim=TRUE,
+                                                cell=TRUE, value=TRUE))
+CRU_TMx_Completa <- CRU_TMx_tbbl %>%
+  left_join(capas_TMx_CRU_stk, by = "dimindex") %>%
+  relocate(CellID = cellindex, CapaID = dimindex, Long = x, Lat = y, Anho, Mes, TMx =
+             cellvalue)
+
+# Seleccionar solo los campos de año, mes y temperatura, exportarlo a csv
+CRU_SerT_TMx <- dplyr::select(CRU_TMx_Completa, -CapaID, -Long, -Lat)
+write_csv(CRU_SerT_TMx, "/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_SerT_TMx.csv")
+
+
+
+##### Precipitación-----------------------------------------------------------------------------------------------------
 
 # Llamar a las capas raster de Chelsa para precipitación y crear una lista: Precipitación (Prec)
 path_Prec <- list.files("/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Geodatos/Mundo/Chelsa_v21_month/pr",
@@ -106,7 +193,7 @@ Prec_stk <- raster::stack(path_Prec) # Crear un stack de las capas
 # Recortar el stack de capas Prec a la CRU
 CRU_Prec <- raster::mask(crop(Prec_stk, CRU_LL), CRU_LL)
 plot(CRU_Prec[[1]])
-CRU_Prec_stk <- stack(CRU_Prec)
+CRU_Prec_stk <- stack(CRU_Prec) # Se tiene que convertir el brick en stack para los siguientes procesos
 
 # Generar el csv del índice de capas del stack
 CRU_Prec_capasID <- as_tibble(names(CRU_Prec_stk)) %>%
@@ -133,60 +220,3 @@ CRU_Prec_Completa <- CRU_Prec_tbbl %>%
 CRU_SerT_Prec <- dplyr::select(CRU_Prec_Completa, -CapaID, -Long, -Lat)
 write_csv(CRU_SerT_Prec, "/Users/enriquemartinez/Library/CloudStorage/GoogleDrive-emm@st.ib.unam.mx/Mi unidad/Proyectos/PAPIIT2022_CC_CRU/Analisis/Clima/Archivos_grandes/CRU_SerT_Prec.csv")
 
-
-
-####Create a rute to ggdrive EXAMPLE
-install.packages("googledrive") ##Install package to connect drive and rstudio
-library(googledrive)
-#Accessing to CRU_TMd.csv file in shared folder in EMM drive
-drive_ls("Analisis/Clima/Archivos_grandes") #giving permission to the folder "Archivos_grandes"
-id_tmd<-"1w4Aw4X5cv7XQIwZKMGcz6qBKq1kKlCJA" #using the "<drv_id>" of CRU_TMd.csv
-tas_tmd<-read.csv(paste0("https://docs.google.com/uc?id=", id_tmd, "&export=download"), header =
-                    FALSE, quote = "", sep = '\t') #reading the file in my computer
-x<-as_id("https://drive.google.com/file/d/1w4Aw4X5cv7XQIwZKMGcz6qBKq1kKlCJA/view?
-usp=share_link")
-tas_tmd<-drive_read_raw(x)
-str(tas_tmd)
-#For upload heavy databases
-##drive_upload("~/Library/Mobile
-Documents/com~apple~CloudDocs/CCGS/Proyectos/PAPIIT/Climate_Analisis/Climate/proof_hatch.csv")
-
-
-
-
-
-
-
-####Create a rute to ggdrive EXAMPLE
-
-install.packages("googledrive") ##Install package to connect drive and rstudio
-library(googledrive)
-
-#Accessing to CRU_TMd.csv file in shared folder in EMM drive
-drive_ls("Analisis/Clima/Archivos_grandes") ##giving permission to the folder "Archivos_grandes"
-id_tmd<-"1w4Aw4X5cv7XQIwZKMGcz6qBKq1kKlCJA" #using the "<drv_id>" of CRU_TMd.csv
-tas_tmd<-read.csv(paste0("https://docs.google.com/uc?id=", id_tmd, "&export=download"), header = FALSE, quote = "", sep = '\t') #reading the file in my computer
-
-x<-as_id("https://drive.google.com/file/d/1w4Aw4X5cv7XQIwZKMGcz6qBKq1kKlCJA/view?usp=share_link")
-tas_tmd<-drive_read_string(x, type="csv")
-tas_tmd %>%
-  drive_read_string() %>%
-  read.csv(text = .)
-
-
-temp <- tempfile(fileext = ".zip")
-download.file("https://drive.google.com/file/d/1w4Aw4X5cv7XQIwZKMGcz6qBKq1kKlCJA/view?usp=share_link",
-              temp)
-out <- unzip(temp, exdir = tempdir())
-tas <- read.csv(out[14], sep = ";")
-str(tas)
-
-temp <- tempfile(fileext = ".zip")
-dl <- drive_download(
-  as_id("1w4Aw4X5cv7XQIwZKMGcz6qBKq1kKlCJA"), path = temp, overwrite = TRUE)
-out <- unzip(temp, exdir = tempdir())
-bank <- read.csv(out[14], sep = ";")
-str(bank)
-
-#For upload heavy databases
-##drive_upload("~/Library/Mobile Documents/com~apple~CloudDocs/CCGS/Proyectos/PAPIIT/Climate_Analisis/Climate/proof_hatch.csv")
